ecddc3e32bfb8f118a0e0c79a1bf024ae7beafae
OneDayGinger <hsremnant9@gmail.com>
2022-08-03 07:33:23 +0100

    Add main.cpp

---
+++ main.cpp
@@ -0,0 +1,67 @@
+#include <stdbool.h>
+#include "parse_data.h"
+
+void usage() {
+	printf("syntax: pcap-test <interface>\n");
+	printf("sample: pcap-test wlan0\n");
+}
+
+typedef struct {
+	char* dev_;
+} Param;
+
+Param param  = {
+	.dev_ = NULL
+};
+
+bool parse(Param* param, int argc, char* argv[]) {
+	if (argc != 2) {
+		usage();
+		return false;
+	}
+	param->dev_ = argv[1];
+	return true;
+}
+
+int main(int argc, char* argv[]) {
+	int answer = 0;
+	int i = 1;
+
+	if (!parse(&param, argc, argv))
+		return -1;
+
+	char errbuf[PCAP_ERRBUF_SIZE];
+	pcap_t* pcap = pcap_open_live(param.dev_, BUFSIZ, 1, 1000, errbuf);
+	if (pcap == NULL) {
+		fprintf(stderr, "pcap_open_live(%s) return null - %s\n", param.dev_, errbuf);
+		return -1;
+	}
+
+	printf("====== Packet #%d ======\n\n", i);
+
+	while (true) {
+		struct pcap_pkthdr* header;
+		const u_char* packet;
+		int res = pcap_next_ex(pcap, &header, &packet);
+		if (res == 0) continue;
+		if (res == PCAP_ERROR || res == PCAP_ERROR_BREAK) {
+			printf("pcap_next_ex return %d(%s)\n", res, pcap_geterr(pcap));
+			break;
+		}
+		answer = parse_data(packet, header->caplen);
+
+		switch(answer){
+			case -1:
+			continue;
+			case -2:
+			continue;
+			case 0:
+			printf("====== Packet #%d ======\n\n", ++i);
+
+		}
+
+		
+	}
+
+	pcap_close(pcap);
+}
